92b3c5f67cfb8c4ded8cb6edc0c7b385690ae84e
Dustin Rodda <drodda@codecraftworks.com>
2018-11-15 09:54:28 -0500

    Merging student's work and updating the scene

--- Assets/Scripts/MoveSelector.cs
+++ Assets/Scripts/MoveSelector.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -8,37 +8,27 @@ public class MoveSelector : MonoBehaviour
     public GameObject tileHighlightPrefab;
     public GameObject attackLocationPrefab;
 
-    private GameObject
-        tileHighlight;
-    private GameObject
-       movingPiece;
+    private GameObject tileHighlight;
+    private GameObject movingPiece;
+    private List<Vector2Int> moveLocations;
+    private List<GameObject> locationHighlights;
 
-        
     // Use this for initialization
     void Start ()
     {
-        this.enabled = false;
-        tileHighlight = Instantiate(tileHighlightPrefab,
-        Geometry.PointFromGrid(new Vector2Int(0, 0)),
-         Quaternion.identity, gameObject.transform);
+        enabled = false;
+        tileHighlight = Instantiate(tileHighlightPrefab, Geometry.PointFromGrid(new Vector2Int(0, 0)), Quaternion.identity, gameObject.transform);
         tileHighlight.SetActive(false);
 	}
-
-	public void EnterState(GameObject piece)
-    {
-
-        movingPiece = piece;
-        this.enabled = true;
-
-    }
+	
 	// Update is called once per frame
 	void Update ()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-        RaycastHit hit;
-        if (Physics.Raycast(ray, out hit))
-        {
 
+        RaycastHit hit;
+        if(Physics.Raycast(ray, out hit))
+        {
             Vector3 point = hit.point;
             Vector2Int gridPoint = Geometry.GridFromPoint(point);
 
@@ -46,28 +36,58 @@ public class MoveSelector : MonoBehaviour
             tileHighlight.transform.position = Geometry.PointFromGrid(gridPoint);
             if (Input.GetMouseButtonDown(0))
             {
+                if (!moveLocations.Contains(gridPoint))
+                {
+                    return;
+                }
                 if (GameManager.instance.PieceAtGrid(gridPoint) == null)
                 {
                     GameManager.instance.Move(movingPiece, gridPoint);
-                    
                 }
                 ExitState();
             }
-
         }
         else
         {
             tileHighlight.SetActive(false);
+        }
+	}
 
+    public void EnterState(GameObject piece)
+    {
+        movingPiece = piece;
+        enabled = true;
+        moveLocations = GameManager.instance.MovesForPiece(movingPiece);
+        locationHighlights = new List<GameObject>();
+
+        foreach (Vector2Int loc in moveLocations)
+        {
+            GameObject highlight;
+            if (GameManager.instance.PieceAtGrid(loc))
+            {
+                highlight = Instantiate(attackLocationPrefab, Geometry.PointFromGrid(loc),
+                    Quaternion.identity, gameObject.transform);
+            }
+            else
+            {
+                highlight = Instantiate(moveLocationPrefab, Geometry.PointFromGrid(loc),
+                    Quaternion.identity, gameObject.transform);
+            }
+            locationHighlights.Add(highlight);
         }
     }
+
     private void ExitState()
     {
-        this.enabled = false;
+        enabled = false;
         tileHighlight.SetActive(false);
         GameManager.instance.DeselectPiece(movingPiece);
         movingPiece = null;
         TileSelector selector = GetComponent<TileSelector>();
         selector.EnterState();
+        foreach (GameObject highlight in locationHighlights)
+        {
+            Destroy(highlight);
+        }
     }
 }

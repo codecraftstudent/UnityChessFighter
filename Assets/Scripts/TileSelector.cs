65a5b477ede287d144930382a6e52743d83cd11a
Codecraft <github@codecraftworks.com>
2018-12-12 17:55:07 -0500

    fix

--- Assets/Scripts/TileSelector.cs
+++ Assets/Scripts/TileSelector.cs
@@ -1,52 +1,24 @@
-﻿/*
- * Copyright (c) 2018 Razeware LLC
- * 
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- * 
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * Notwithstanding the foregoing, you may not use, copy, modify, merge, publish, 
- * distribute, sublicense, create a derivative work, and/or sell copies of the 
- * Software in any work that is designed, intended, or marketed for pedagogical or 
- * instructional purposes related to programming, coding, application development, 
- * or information technology.  Permission for such use, copying, modification,
- * merger, publication, distribution, sublicensing, creation of derivative works, 
- * or sale is expressly withheld.
- *    
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- */
-
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class TileSelector : MonoBehaviour
 {
     public GameObject tileHighlightPrefab;
-
     private GameObject tileHighlight;
 
+
+    // Use this for initialization
     void Start ()
     {
-        Vector2Int gridPoint = Geometry.GridPoint(0, 0);
-        Vector3 point = Geometry.PointFromGrid(gridPoint);
+        Vector2Int gridpoint = Geometry.GridPoint(0, 0);
+        Vector3 point = Geometry.PointFromGrid(gridpoint);
         tileHighlight = Instantiate(tileHighlightPrefab, point, Quaternion.identity, gameObject.transform);
         tileHighlight.SetActive(false);
-    }
-
-    void Update ()
+	}
+	
+	// Update is called once per frame
+	void Update ()
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
 
@@ -58,13 +30,13 @@ public class TileSelector : MonoBehaviour
 
             tileHighlight.SetActive(true);
             tileHighlight.transform.position = Geometry.PointFromGrid(gridPoint);
+
             if (Input.GetMouseButtonDown(0))
             {
                 GameObject selectedPiece = GameManager.instance.PieceAtGrid(gridPoint);
                 if (GameManager.instance.DoesPieceBelongToCurrentPlayer(selectedPiece))
                 {
                     GameManager.instance.SelectPiece(selectedPiece);
-                    // Reference Point 1: add ExitState call here later
                     ExitState(selectedPiece);
                 }
             }
@@ -73,7 +45,7 @@ public class TileSelector : MonoBehaviour
         {
             tileHighlight.SetActive(false);
         }
-    }
+	}
 
     public void EnterState()
     {
